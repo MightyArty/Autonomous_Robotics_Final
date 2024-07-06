2b5c46afbedd5fba09e6870726368edb64f9cf24
Tom Shabalin <tomshabalin95@gmail.com>
2024-07-06 21:53:05 +0300

    Added all the sonesors for now, not checked yet.

--- Utils.py
+++ Utils.py
@@ -3,9 +3,43 @@ This file represent all the classes and functions that are used in the Drone 2D 
 Such as the Battery sensor, Optical Flow sensor, Distance sensor and Process ID.
 """
 
-class ProcessID:
-    def __init__(self, id):
-        self.id = id
+import math
+
+# Proportional-Integral-Derivative
+class DronePIDController:
+    def __init__(self, kp, ki, kd, max_integral):
+        self.integral: float = 0.0
+        self.last_error: float = 0.0
+        self.first_run: bool = True
+        self.kd = kd
+        self.kp = kp
+        self.ki = ki
+        self.max_integral = max_integral
+    
+    def update(self, error, dt):
+        if self.first_run:
+            self.first_run = False
+            self.last_error = error
+
+        self.integral += self.ki * error * dt
+        self.integral = max(min(self.integral, self.max_integral), -self.max_integral)
+        
+        derivative = 0.0
+        if dt != 0:
+            derivative = (error - self.last_error) / dt
+        control_output = self.kp * error + self.kd * derivative + self.integral
+        
+        self.last_error = error
+        return control_output
+    
+    def increment_kp(self, value):
+        self.kp += value
+
+    def increment_ki(self, value):
+        self.ki += value
+
+    def increment_kd(self, value):
+        self.kd += value
 
 class BatterySensor:
     """
@@ -13,7 +47,7 @@ class BatterySensor:
     """
     def __init__(self, battery_level:int = 100):
         self._battery_level = battery_level
-        self._total_time = 4800 # Max time of flight 480 * 10 updates per second
+        self._total_time: int = 4800 # Max time of flight 480 * 10 updates per second
         self._time_passed = self._total_time
 
     def update_battery_level(self):
@@ -48,9 +82,9 @@ class OpticalSensor:
     Optical Sensor class that represents the optical sensor of the drone.
     """
     def __init__(self):
-        self._maxSpeed = 3
-        self._currentSpeed = 0
-        self._a = 1 # acceleration
+        self._maxSpeed: int = 3
+        self._currentSpeed: int = 0
+        self._a: float = 1 # acceleration
 
     def update_speed_a(self):
         """
@@ -71,5 +105,59 @@ class OpticalSensor:
             self._currentSpeed -= 0
 
 class DistanceSensor:
-    def __init__(self, distance):
-        self.distance = distance
+    """
+    Distance Sensor class that represents the distance sensor of the drone.
+    """
+    directions_dict = {"leftward": 270, "rightward": 90, "forward": 0, "backward": 180, "forward_right_diagonal": 40, "forward_left_diagonal": 320}
+
+    def __init__(self, direction, distance:int = 0):
+        self._droneGG: int = 120
+        self._distance = distance
+        self._direction = direction
+
+    def distance_update(self, radius, matrix, location, orientation):
+        """
+        Update the distance of the drone.
+
+        Parameters
+        --------
+        `radius`: The radius of the drone.
+        `matrix`: The matrix representing the Map.
+        `location`: The location of the drone.
+        `orientation`: The orientation of the drone.
+        """
+        angle = orientation + self.directions_dict[self._direction]
+        x = math.cos(math.radians(angle))
+        y = math.sin(math.radians(angle))
+        dx, dy = location
+
+        if x < 0:
+            sensor_x = dx - radius
+        else:
+            sensor_x = dx + radius
+        if y < 0:
+            sensor_y = dy - radius
+        else:
+            sensor_y = dy + radius
+
+        count_distance = 1
+        matrix_length = len(matrix)
+        matrix_width = len(matrix[0])
+        while count_distance <= self._droneGG + 1:
+            num_y = int(sensor_y + y * count_distance)
+            num_x = int(sensor_x + x * count_distance)
+            if num_y < 0 or num_x < 0 or num_x >= matrix_width or num_y >= matrix_length:
+                self._distance = count_distance * 2.5
+                return
+            if matrix[num_y][num_x] == 1:
+                self._distance = count_distance * 2.5
+                return
+            count_distance += 1
+        self._distance = self._droneGG * 2.5
+
+class InertialMeasurementUnit:
+    """
+    Inertial Measurement Unit class that represents the IMU of the drone.
+    """
+    def __init__(self, start_orientation:int = 0):
+        self.drone_orientation = start_orientation
